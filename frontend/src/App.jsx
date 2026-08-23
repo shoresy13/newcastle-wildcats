a811160636212f4e7bf1d8a0fcf44fa91490c63f
Jacob Lewis-Shores <jacoblewisshores@gmail.com>
2026-08-23 15:44:28 +0100

    Configure react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,46 @@
-import { useEffect } from 'react';
-import { checkConnection } from './utils/api';
+import React, { useEffect } from "react";
+import { Routes, Route, Outlet, useLocation } from "react-router";
+import { checkConnection } from "./utils/api";
 
-function App() {
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
   useEffect(() => {
-    checkConnection()
-        .then((data) => console.log('Success:', data))
-        .catch((err) => console.error('Error:', err));
-  }, []);
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
+  return null;
+}
+
+export function Layout() {
   return (
-      <>
-      </>
+      <div>
+        <ScrollToTop />
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
   );
 }
 
-export default App;
+export default function App() {
+  useEffect(() => {
+    checkConnection()
+        .then((data) => console.log("Success:", data))
+        .catch((err) => console.error("Error:", err));
+  }, []);
+
+  return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+  );
+}
